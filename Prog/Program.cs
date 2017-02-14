a18e0b31a63b67cd1dcaa1bfc945beb1b34bf10e
Branoli <lar.pasha.chenko@gmail.com>
2017-02-14 22:19:35 +0300

    Polygon v2.0
    Methods transferred in classes

--- Prog/Program.cs
+++ Prog/Program.cs
@@ -58,107 +58,119 @@ namespace Хоар
                 }
                 for (int i = 0; i < edges.Length; i++)
                 {
-                    this.AB[i] = edges[i];   
+                    this.AB[i] = edges[i];
                 }
             }
-        }
-        //==========METHODS=========
-        static void Distance(Polygon N)
-        {
-            double q, z = 0;
-            for (int i = 0; i < N.AB.Length; i++)
+            public void AREA(Triangle[] ABC)
             {
-                z++;
-                q = Math.Sqrt(Math.Pow(N.AB[i].b.x - N.AB[i].a.x, 2) + Math.Pow(N.AB[i].b.y - N.AB[i].a.y, 2));
-                Console.WriteLine("{0}ое ребро = {1}",z,q);
-            }
-        }
-        public static double Area(Polygon N)
-        {
-            double v = 0;
-            double[] mas = new double[3];
-            for (int i = 0; i < N.ABC.Length; i++)
-            {
-                double c = 1, p = 0, q = 0, f = 0;
-                for (int k = 0; k < N.ABC[i].a.Length; k++)
+                double v = 0, d = 0, SumPerimeter = 0;
+                for (int i = 0; i < ABC.Length; i++)
                 {
-                    if (k == N.ABC[i].a.Length - 1)
+                    int[] mas = new int[3];
+                    int[] CheckPerimeter = new int[3];
+                    int q;
+                    double c = 1, p = 0, f = 0, m = 0, OneOrZeroOfPerimeter = 0, Perimeter = 0;
+                    for (int k = 0; k < ABC[i].a.Length; k++)
                     {
-                        q = Math.Sqrt(Math.Pow(N.ABC[i].a[k - 2].x - N.ABC[i].a[k].x, 2) + Math.Pow(N.ABC[i].a[k - 2].y - N.ABC[i].a[k].y, 2));
-                        mas[k] = q;
-                        p += q;
+                        if (k == ABC[i].a.Length - 1)
+                        {
+                            q = (int)Math.Sqrt(Math.Pow(ABC[i].a[k - 2].x - ABC[i].a[k].x, 2) + Math.Pow(ABC[i].a[k - 2].y - ABC[i].a[k].y, 2));
+                            mas[k] = q;
+                            CheckPerimeter[k] = q;
+                            p += q;
+                            Perimeter += q;
+                        }
+                        else
+                        {
+                            q = (int)Math.Sqrt(Math.Pow(ABC[i].a[k + 1].x - ABC[i].a[k].x, 2) + Math.Pow(ABC[i].a[k + 1].y - ABC[i].a[k].y, 2));
+                            mas[k] = q;
+                            CheckPerimeter[k] = q;
+                            p += q;
+                            Perimeter += q;
+                        }
                     }
-                    else
+                    p /= 2;
+                    for (int k = 0; k < mas.Length; k++)
                     {
-                        q = Math.Sqrt(Math.Pow(N.ABC[i].a[k + 1].x - N.ABC[i].a[k].x, 2) + Math.Pow(N.ABC[i].a[k + 1].y - N.ABC[i].a[k].y, 2));
-                        mas[k] = q;
-                        p += q;
+                        f = p - mas[k];
+                        c = c * f;
                     }
-                }
-                p /= 2;
-                for (int k = 0; k < mas.Length; k++)
-                {
-                    f = p + mas[k];
-                    c = c * f;
-                }
-                p = p * c;
-                p = Math.Sqrt(p);
-                v = v + p;
-            }
-            return v;
+                    p = p * c;
+                    p = Math.Sqrt(p);
+                    v += p;
+                    m = Isosceles(mas);
+                    OneOrZeroOfPerimeter = Right(CheckPerimeter);
+                    if (m == 1) d += p;
+                    if (OneOrZeroOfPerimeter == 1) SumPerimeter += p;
 
-        }
-        public static double Perimeter(Polygon N)
-        {
-            double q = 0;
-            for (int i = 0; i < N.AB.Length; i++)
+                }
+                if (d != 0) Console.WriteLine("Площадь фигуры = {0} \nСредняя площадь прямоугольных треугольников в этом многоугольнике = {1}", v, d/2);
+                else Console.WriteLine("Площадь фигуры = {0}, в многоугольнике нет равнобедренных треугольников ", v);
+                if (SumPerimeter != 0) Console.WriteLine("\nСредний периметр всех прямоугольных треугольников в этом многоугольнике = {0}", SumPerimeter / 2);
+                else Console.WriteLine("==-==\nВ многоугольнике нет прямоугольных треугольников ");
+
+            }
+            public void Perimeter(Edge[] AB)
             {
-                q+= Math.Sqrt(Math.Pow(N.AB[i].b.x - N.AB[i].a.x, 2) + Math.Pow(N.AB[i].b.y - N.AB[i].a.y, 2));
+                int q, PerimeterOfPolygon = 0;
+                for (int i = 0; i < AB.Length; i++)
+                {
+                    q = (int)Math.Sqrt(Math.Pow(AB[i].b.x - AB[i].a.x, 2) + Math.Pow(AB[i].b.y - AB[i].a.y, 2));
+                    PerimeterOfPolygon += q;
+                }
+                Console.WriteLine("Периметр многоугольника = {0}", PerimeterOfPolygon);
             }
-            return q;
-        }
-        //public static double Isosceles(Polygon N)
-        //{
-            
-        //}
-        //}
-        //public static double Right(Triangle ABC)
-        //{
-        //    double q, w, e;
-        //    q = Math.Sqrt(Math.Pow(ABC.b.x - ABC.a.x, 2) + Math.Pow(ABC.b.y - ABC.a.y, 2));
-        //    w = Math.Sqrt(Math.Pow(ABC.c.x - ABC.b.x, 2) + Math.Pow(ABC.c.y - ABC.b.y, 2));
-        //    e = Math.Sqrt(Math.Pow(ABC.a.x - ABC.c.x, 2) + Math.Pow(ABC.a.y - ABC.c.y, 2));
-        //    if (Math.Pow(q,2) == Math.Pow(w,2)+Math.Pow(e,2))
-        //    {
-        //        double i = 1;
-        //        return i;
-        //    }
-        //    if (Math.Pow(w,2) == Math.Pow(q,2)+Math.Pow(e,2))
-        //    {
-        //        double i = 1;
-        //        return i;
-        //    }
-        //    if (Math.Pow(e, 2) == Math.Pow(q, 2) + Math.Pow(w, 2))
-        //    {
-        //        double i = 1;
-        //        return i;
-        //    }
-        //    else
-        //    {
-        //        double i = 0;
-        //        return i;
-        //    }
+            public void Distance(Edge[] AB)
+            {
+                int q, z = 0;
+                for (int i = 0; i < AB.Length; i++)
+                {
+                    z++;
+                    q = (int)Math.Sqrt(Math.Pow(AB[i].b.x - AB[i].a.x, 2) + Math.Pow(AB[i].b.y - AB[i].a.y, 2));
+                    Console.WriteLine("{0}ое ребро = {1}", z, q);
+                }
+            }
+            public static int Right(int[] Edges)
+            {
+                if (Math.Pow(Edges[0], 2) == Math.Pow(Edges[0], 2) + Math.Pow(Edges[0], 2))
+                {
+                    int i = 1;
+                    return i;
+                }
+                if (Math.Pow(Edges[1], 2) == Math.Pow(Edges[0], 2) + Math.Pow(Edges[2], 2))
+                {
+                    int i = 1;
+                    return i;
+                }
+                if (Math.Pow(Edges[2], 2) == Math.Pow(Edges[0], 2) + Math.Pow(Edges[1], 2))
+                {
+                    int i = 1;
+                    return i;
+                }
+                else
+                {
+                    int i = 0;
+                    return i;
 
-        //}
-        //==========================
+                }
+            }
+            public static int Isosceles(int[] Edges)
+            {
+                if (Edges[0] == Edges[1] || Edges[0] == Edges[2] || Edges[1] == Edges[2]) return 1;
+                else return 0;
+            }
+        }
         static void Main(string[] args)
         {
-            //+++++++++++++++Massiv Triagles+++++++++++++++++++++++
+            Console.Write("Скольки угольник вас интерисует: ");
+            int q = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            //+++++++++++++++Massiv Polygon+++++++++++++++++++++++
             Random gen = new Random();
             Polygon[] N = new Polygon[1];
-            Triangle[] abc = new Triangle[1];      
+            Triangle[] abc = new Triangle[q - 2];      
             Point[] points = new Point[3];
-            Edge[] edges = new Edge[3];
+            Edge[] edges = new Edge[q];
             int x = 0, z = 0;
             for (int p = 0; p < N.Length; p++)
             {
@@ -168,7 +180,7 @@ namespace Хоар
                     {
                         for (int i = 0; i < points.Length; i++)
                         {
-                            points[i] = new Point(gen.Next(0, 5), gen.Next(0, 5));
+                            points[i] = new Point(gen.Next(0, 10), gen.Next(0, 10));
                         }
                         for (int k = 0; k < edges.Length; k++)
                         {
@@ -207,24 +219,16 @@ namespace Хоар
                     }
 
                 }
-                edges[edges.Length - 1] = new Edge(edges[0].a, edges[edges.Length - 2].b);
+                edges[edges.Length - 1] = new Edge(edges[edges.Length - 2].b, edges[0].a );
                 N[p] = new Polygon(abc, edges);
             }               
             
             //==========OUTPUT==============================
             for (int i = 0; i < N.Length; i++)
             {
-
-                //if (Isosceles(abc[i]) == 1) Console.WriteLine("Isosceles");
-                //else Console.WriteLine("Not Isosceles");
-
-                //if (Right(abc[i]) == 1) Console.WriteLine("Right");
-                //else Console.WriteLine("Not Right");
-
-                Distance(N[i]);
-
-                Console.WriteLine("Периметр = {0}",Perimeter(N[i]));
-                Console.WriteLine("Площадь = {0}",Area(N[i]));
+                N[i].Distance(N[i].AB);
+                N[i].AREA(N[i].ABC);
+                N[i].Perimeter(N[i].AB);
                 Console.WriteLine("====-----------------====");
             }
             Console.ReadKey();
