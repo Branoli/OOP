65f1dad2af1f9a59ae4adcb3ddb8fb1117084449
Branoli <lar.pasha.chenko@gmail.com>
2017-02-25 10:42:23 +0300

    first commit

---
+++ Game/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Program
+    {
+        class Square
+        {
+            int x;
+            public Square(int x)
+            {
+                this.x = x;
+            }
+        }
+        class Box
+        {
+            public Square[] points;
+            public Box(Square[] number)
+            {
+                this.points = new Square[16];
+
+                for (int i = 0; i < points.Length; i++)
+                    points[i] = number[i];
+            }
+        }
+        static void Main(string[] args)
+        {
+            Box[] box = new Box[1];
+            Square[] number = new Square[16];
+            Random gen = new Random();
+            int q = 1;
+            for (int i = 0; i < box.Length; i++)
+            {
+                for (int j = 0; j < number.Length; j++)
+                {
+                    if (j == 15)
+                    {
+                        number[j] = new Square(0);
+                    }
+                    else
+                    {
+                        number[j] = new Square(q);
+                        q++;
+                    }
+                }
+                box[i] = new Box(number);
+            }
+                
+            Console.ReadKey();
+
+        }
+    }
+}
--- Prog/Point.cs
+++ Prog/Point.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Prog
 {
-    public class Point
+    class Point
     {
         public int x;
         public int y;
@@ -14,33 +14,15 @@ namespace Prog
         {
             this.x = x;
             this.y = y;
+            GenerationOfPoints(x, y);
         }
-        public static Point[] GenerationOfPoints(Point[] points, int x)
+        public Point GenerationOfPoints(int x, int y)
         {
             Random gen = new Random();
-            if (x == 0)
-            {
-                do
-                {
-                    for (int i = 0; i < points.Length; i++)
-                    {
-                        points[i] = new Point(gen.Next(0, 10), gen.Next(0, 10));
-                    }
-                } while (points[0].y == points[1].y && points[0].y == points[2].y && points[1].y == points[2].y);
-            }
-            else
-            {
-                if (points[2].x == points[1].x && points[2].y == points[1].y || points[2].x == points[0].x && points[2].y == points[0].y)
-                {
-                    do
-                    {
-                        points[2] = new Point(gen.Next(0, 10), gen.Next(0, 10));
-                    } while (points[2].x == points[1].x && points[2].y == points[1].y || points[2].x == points[0].x && points[2].y == points[0].y);
-                    x++;
-                    GenerationOfPoints(points, x);
-                }
-            }
-            return points;
+            x = gen.Next(0, 10);
+            y = gen.Next(0, 10);
+            Point a = new Point(x, y);
+            return a;
         }
     }
 }
