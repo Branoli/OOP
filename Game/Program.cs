7c87bb9a1c069c2a9c97c11198b34b5643254621
Branoli <lar.pasha.chenko@gmail.com>
2017-03-03 22:38:44 +0300

    Game v 1.0

--- Game/Program.cs
+++ Game/Program.cs
@@ -8,46 +8,9 @@ namespace Game
 {
     class Program
     {
-        class Square
-        {
-            int x;
-            public Square(int x)
-            {
-                this.x = x;
-            }
-        }
-        class Box
-        {
-            public Square[] points;
-            public Box(Square[] number)
-            {
-                this.points = new Square[16];
-
-                for (int i = 0; i < points.Length; i++)
-                    points[i] = number[i];
-            }
-        }
         static void Main(string[] args)
         {
-            Box box;
-            Square[] number = new Square[16];
-            Random gen = new Random();
-            int q = 1;
-            for (int j = 0; j < number.Length; j++)
-            {
-                if (j == 15)
-                {
-                    number[j] = new Square(0);
-                }
-                else
-                {
-                    number[j] = new Square(q);
-                    q++;
-                }
-            }
-            box = new Box(number);
-
-
+            Knuckles knuckles = new Knuckles();
 
 
             Console.ReadKey();
